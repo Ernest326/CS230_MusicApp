95973e496f176e3bfc4146091bd3b3c7adb38848
Ernest326 <ernest326648@gmail.com>
2025-03-18 14:58:51 +0000

    Added POST, PUT, DELETE methods

--- packages/backend/controllers/artistController.js
+++ packages/backend/controllers/artistController.js
@@ -11,3 +11,43 @@ exports.getAllArtists = (req, res) => {
         res.send(rows)
     });
 };
+
+exports.createArtist = (req, res) => {
+    console.log("BACKEND: Received request createArtist");
+    const { name, listeners, genre } = req.body;
+    db.query('INSERT INTO ARTIST (name, monthly_listeners, genre) VALUES (?, ?, ?)', [name, listeners, genre], (err, result) => {
+        if (err) {
+            console.log("BACKEND: Error in createArtist");
+            res.sendStatus(500);
+            throw err;
+        }
+        res.send(result)
+    });
+}
+
+exports.updateArtist = (req, res) => {
+    console.log("BACKEND: Received request updateArtist");
+    const id = req.params.id;
+    const { name, listeners, genre } = req.body;
+    db.query('UPDATE ARTIST SET name = ?, monthly_listeners = ?, genre = ? WHERE name = ?', [name, listeners, genre, id], (err, result) => {
+        if (err) {
+            console.log("BACKEND: Error in updateArtist");
+            res.sendStatus(500);
+            throw err;
+        }
+        res.send(result)
+    });
+}
+
+exports.deleteArtist = (req, res) => {
+    console.log("BACKEND: Received request deleteArtist");
+    const id = req.params.id;
+    db.query('DELETE FROM ARTIST WHERE name = ?', [id], (err, result) => {
+        if (err) {
+            console.log("BACKEND: Error in deleteArtist");
+            res.sendStatus(500);
+            throw err;
+        }
+        res.send(result)
+    });
+}
